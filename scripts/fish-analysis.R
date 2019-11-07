4e8e0909b52118636513329982eb4e57b79db0a6
dljones4321 <dljones421@q.com>
2019-11-07 15:05:07 -0800

    Added the column "length_cat"

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -6,5 +6,9 @@ fish_data
 # Building a history
 
 library(dplyr)
+
+# The line below creates a new data set with the Variable "length_cat" that has the value "big" if the length is over 300 and "small" if the value is less than 300
+
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+fish_data_cat
